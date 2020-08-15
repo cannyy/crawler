93fe61dba9b171844683edab6990776dd7273709
Canny <hl6349@gmail.com>
2020-08-15 10:12:33 +0800

    08151

--- practise1/pachong3.py
+++ practise1/pachong3.py
@@ -1,40 +1,38 @@
 import requests
 import bs4
-
-
 import xlwings as xw
-app=xw.App(visible=True,add_book=False)
-app.display_alerts=False
-app.screen_updating=False
-# 文件位置：filepath，打开test文档，然后保存，关闭，结束程序
-filepath=r'test1.xlsx'
-wb=app.books.open(filepath)
+import time
 
-r=requests.get("http://zrzy.jiangsu.gov.cn/nt/gtzx/gzdt/")
-html_doc=r.content.decode('utf-8')
-soup = bs4.BeautifulSoup(html_doc,'lxml')
-a=len(soup.select('a[target="_blank"]'))
-zong=[]
-for cishu in range(0,a-1,1):
-    zong=zong.append(soup.select('a[target="_blank"]')[cishu]['href'])
+# 打开Excel程序，默认设置：程序可见，只打开不新建工作薄
+app = xw.App(visible=True,add_book=False)
+# 新建工作簿 (如果不接下一条代码的话，Excel只会一闪而过，卖个萌就走了）
+wb = app.books.add()
 
-n=1
-for cishu in range(0,a-1,1):
-    print(cishu)
-    dingwei=zong[cishu]
+# 打开已有工作簿（支持绝对路径和相对路径）
+# wb = app.books.open('example.xlsx')
 
-    r2=requests.get("http://zrzy.jiangsu.gov.cn/nt/gtzx/gzdt/"+dingwei)
-    html_doc=r2.content.decode('utf-8')
-    soup = bs4.BeautifulSoup(html_doc,'lxml')
-    c=soup.find_all("p")
+wb = xw.Book('test1.xlsx')
+
+r = requests.get("http://zrzy.jiangsu.gov.cn/nt/gtzx/gzdt/")
+html_doc = r.content.decode('utf-8')
+soup = bs4.BeautifulSoup(html_doc, 'lxml')
+a = len(soup.select('a[target="_blank"]'))
+zong = soup.select('a[target="_blank"]')
+n = 1
+for cishu in range(0, a - 1, 1):
+    fen = zong[cishu].attrs["href"]
+
+    dingwei = fen
+
+    r2 = requests.get("http://zrzy.jiangsu.gov.cn/nt/gtzx/gzdt/" + dingwei)
+    html_doc = r2.content.decode('utf-8')
+    soup = bs4.BeautifulSoup(html_doc, 'lxml')
+    c = soup.find_all("p")
 
     for i in c:
-        wb.sheets['sheet1'].range('A'+str(n)).value=str(i.text)
-        n=n+1
-
-
+        wb.sheets['sheet1'].range('A' + str(n)).value = str(i.text)
+        n = n + 1
 
 wb.save()
 wb.close()
 app.quit()
-
